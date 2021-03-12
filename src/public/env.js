eacd4cd08a63adcdc77097a4b735bc8e0d87c260
jideabdqudus <jideabdqudus@gmail.com>
2021-03-12 17:35:54 +0100

    Removed APPUrl

--- src/public/env.js
+++
@@ -1,5 +0,0 @@
-window.env = {
-  REACT_APP_BASE_URL: '#{URL}', //check octopus to confirm
-  INACTIVITY_TIMEOUT: 90000,
-};
-
--- src/src/constants/app.constants.js
+++
@@ -1,8 +0,0 @@
-export const appConstants={
-  REACT_APP_BASE_URL: process.env.NODE_ENV === "development" ? "https://trail-api.test.vggdev.com" : window.env.appUrl, //check octopus
-
-  SDGS:"sdgs",
-  ALL:"all",
-  INDICATORS:"indicators",
-  PROGRAMS:"programs"
-} 
